d21f8d956810f59df8bf684f26a2187658d6db43
jmmclaug201 <mclaughlinj201@gmail.com>
2024-11-29 18:03:06 -0500

    Disclaimers

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,22 @@
 classifications = document.getElementById("classifications");
 button = document.getElementById("classify-button");
 
+const WARNING =  
+  `<div id="warning">
+    We're not too confident about these classifications, try adding more words
+    to your post or checking these subreddits out. 
+  </div>`;
+
+const BAR = (subreddit, percentage) => {
+  return `
+    <div class="classification">
+      <p><a href="https://www.reddit.com/r/${subreddit}/">r/${subreddit}</a> - ${Math.round(percentage * 1000) / 10}%</p>
+      <div class="subreddit-bar">
+        <div class="fill" style="width: ${100 * percentage}%"></div>
+      </div>
+    </div>`;
+}
+
 button.addEventListener("click", async (e) => {
   classifications.replaceChildren();
 
@@ -10,17 +26,14 @@ button.addEventListener("click", async (e) => {
   const json = await response.json();
   
   subreddits = Object.entries(json.prediction).sort(([s1, p1], [s2, p2]) => {return p2 - p1;})
+  if (subreddits[0][1] <= 0.2) {
+    classifications.insertAdjacentHTML("beforeend", WARNING);
+  }
   subreddits.forEach(([subreddit, percentage]) => {
     classifications.insertAdjacentHTML(
-      "beforeend",
-      `<div class="classification">
-        <p><a href="https://www.reddit.com/r/${subreddit}/">r/${subreddit}</a> - ${Math.round(percentage * 1000) / 10}%</p>
-        <div class="subreddit-bar">
-          <div class="fill" style="width: ${100 * percentage}%"></div>
-        </div>
-      </div>`
+      "beforeend", BAR(subreddit, percentage)
     );
   });
 
-  classifications.scrollIntoView(scrollIntoViewOptions={behavior: 'smooth'})
+  classifications.scrollIntoView(scrollIntoViewOptions={behavior: 'smooth'});
 })
